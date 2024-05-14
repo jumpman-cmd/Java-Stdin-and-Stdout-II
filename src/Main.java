f58f2b7a6c0355effcd5a2593b6ad3d7f73f9ad6
jumpman-cmd <codeks7407@gmail.com>
2024-05-14 21:32:03 +0200

    Initial Commit

---
+++ src/Main.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class Main
+{
+
+    public static void main(String[] args)
+    {
+        Scanner scanner = new Scanner(System.in);
+
+        int i = scanner.nextInt();
+        double d = scanner.nextDouble();
+        scanner.nextLine(); // Consume the newline character
+        String s = scanner.nextLine();
+
+        System.out.println("String: " + s);
+        System.out.println("Double: " + d);
+        System.out.println("Int: " + i);
+    }
+}
